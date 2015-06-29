516294936a650fa037cd973605dec0f91b79a706
Gray Smith <gray.smith00@gmail.com>
2015-06-29 15:59:50 +0000

    added validations and create action

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -5,7 +5,19 @@ class ContactsController < ApplicationController
   end
   
   def create
+    @contact = Contact.new(contact_params)
+    if @contact.save
+      flash[:success] = "Your contact form has been submitted"
+      redirect_to new_contact_path
+    else
+      flash[:danger] = "An Error has occured, message has not been sent"
+      redirect_to new_contact_path
+    end
   end
   
+  private
+    def contact_params
+      params.require(:contact).permit(:name, :email, :comments)
+    end
   
 end
