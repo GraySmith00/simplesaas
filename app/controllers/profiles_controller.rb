c0e1c41d8680c8b2bb5a9bcaa416ffd18dc09693
Gray Smith <gray.smith00@gmail.com>
2015-07-01 18:11:23 +0000

    modified new action in profiles controller and changed view

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,7 +3,7 @@ class ProfilesController < ApplicationController
   def new
     # form for each user to fill out their profile
     @user = User.find( params[:user_id] )
-    @profile = @user.build_profile
+    @profile = Profile.new
   end
   
   def create
