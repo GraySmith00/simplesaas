388caf9afff89f2fb6d366d3fc92cb12b1e3e30a
Gray Smith <gray.smith00@gmail.com>
2015-07-01 17:20:27 +0000

    added profiles new action and fixed profile.rb

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,3 +1,9 @@
 class ProfilesController < ApplicationController
   
+  def new
+    # form for each user to fill out their profile
+    @user = User.find(params[:user_id])
+    @profile = @user.build_profile
+  end
+  
 end
