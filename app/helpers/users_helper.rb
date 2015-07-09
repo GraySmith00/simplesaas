21fa58299380a29a70573e8899325ec7671c154b
Gray Smith <gray.smith00@gmail.com>
2015-07-09 15:33:08 +0000

    added index action to users controller and updated link

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -7,8 +7,7 @@ module UsersHelper
       "<i class='fa fa-lightbulb-o'></i>".html_safe
     elsif @user.profile.job_title == "Investor"
       "<i class='fa fa-dollar'></i>".html_safe
-    end
-    
+    end  
   end
   
 end
